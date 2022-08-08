d23701785fafb5c124f4031b18f1a79119edcc7a
DLionApps <darshanadilhan09@gmail.com>
2022-08-08 21:32:36 +0530

    intial commit

---
+++ CustomMidleware/CustomMidleware/Middlewares/ReqestOriginMidleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace CustomMidleware.Middlewares
+{
+    public class ReqestOriginMidleware
+    {
+        private readonly RequestDelegate _next;
+        public ReqestOriginMidleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context, IWebHostEnvironment env)
+        {
+            context.Response.Cookies.Append("ENV", env.EnvironmentName);
+            
+            await _next(context);
+        }
+    }
+}
